2fad0642d3897a615118a1052666edc509fad74c
marnixah <marnix@roboco.dev>
2023-02-26 14:06:44 +0100

    change methods to public ones for ease of use

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use async_std::io::WriteExt;
+use reqwest::Response;
 use scraper::Html;
 use scraper::Selector;
 use std::{env, io::Result};
@@ -12,52 +13,60 @@ async fn main() -> Result<()> {
     }
 
     for link in env::args().skip(1) {
-        let audio_links = get_audiolinks_from_rj(&link).await?;
-        for audio_link in audio_links {
-            let filename = audio_link.split("/").last().unwrap();
-            // Check if the file already exists
-            if async_std::fs::metadata(filename).await.is_ok() {
-                println!("{} already exists", filename);
-                continue;
-            }
-
-            let reqwest_result = reqwest::Client::new()
-                .get(&audio_link)
-                .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0")
-                .header("Accept", "audio/webm,audio/ogg,audio/wav,audio/*;q=0.9,application/ogg;q=0.7,video/*;q=0.6,*/*;q=0.5")
-                .header("Accept-Language", "en-US,en;q=0.5")
-                .header("Range", "bytes=0-")
-                .header("Alt-Used", "v.weeab0o.xyz")
-                .header("Connection", "keep-alive")
-                .header("Referer", "https://japaneseasmr.com/")
-                .header("Sec-Fetch-Dest", "audio")
-                .header("Sec-Fetch-Mode", "no-cors")
-                .header("Sec-Fetch-Site", "cross-site")
-                .header("DNT", "1")
-                .header("Sec-GPC", "1")
-                .header("Accept-Encoding", "identity")
-                .header("TE", "trailers")
-                .send()
-                .await;
-            let mut response = match reqwest_result {
+        let responses = reqwest_rj(&link).await?;
+        for response in responses {
+            let mut response = match response {
                 Ok(response) => response,
-                Err(_) => {
-                    println!("Error downloading {}", filename);
+                Err(e) => {
+                    println!("Error: {}", e);
                     continue;
                 }
             };
+            let filename = response
+                .url()
+                .path_segments()
+                .unwrap()
+                .last()
+                .unwrap()
+                .to_string();
 
-            let mut file = async_std::fs::File::create(filename).await?;
+            let mut file = async_std::fs::File::create(&filename).await?;
             while let Some(chunk) = response.chunk().await.unwrap() {
                 file.write_all(&chunk).await.unwrap();
             }
-            println!("Downloaded {}", filename);
+            println!("Downloaded {}", &filename);
         }
     }
     Ok(())
 }
 
-async fn get_audiolinks_from_rj(url: &str) -> Result<Vec<String>> {
+pub async fn reqwest_rj(rj_url: &str) -> Result<Vec<reqwest::Result<Response>>> {
+    let audio_links = get_audiolinks_from_rj(rj_url).await?;
+    let mut responses = Vec::new();
+    for audio_link in audio_links {
+        let response = reqwest::Client::new().get(audio_link).header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0")
+        .header("Accept", "audio/webm,audio/ogg,audio/wav,audio/*;q=0.9,application/ogg;q=0.7,video/*;q=0.6,*/*;q=0.5")
+        .header("Accept-Language", "en-US,en;q=0.5")
+        .header("Range", "bytes=0-")
+        .header("Alt-Used", "v.weeab0o.xyz")
+        .header("Connection", "keep-alive")
+        .header("Referer", "https://japaneseasmr.com/")
+        .header("Sec-Fetch-Dest", "audio")
+        .header("Sec-Fetch-Mode", "no-cors")
+        .header("Sec-Fetch-Site", "cross-site")
+        .header("DNT", "1")
+        .header("Sec-GPC", "1")
+        .header("Accept-Encoding", "identity")
+        .header("TE", "trailers")
+        .send()
+        .await;
+        responses.push(response);
+    }
+
+    Ok(responses)
+}
+
+pub async fn get_audiolinks_from_rj(url: &str) -> Result<Vec<String>> {
     let response = reqwest::get(url).await.unwrap();
     let body = response.text().await.unwrap();
     let document = Html::parse_document(&body);
