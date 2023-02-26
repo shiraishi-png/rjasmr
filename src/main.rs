23addff897ad342123c0d915d3b6a0ab5039ef7e
marnixah <marnix@roboco.dev>
2023-02-26 13:23:47 +0100

    fix cloudflare issues

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,32 @@ async fn main() -> Result<()> {
                 continue;
             }
 
-            let mut response = reqwest::Client::new()
+            let reqwest_result = reqwest::Client::new()
                 .get(&audio_link)
+                .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0")
+                .header("Accept", "audio/webm,audio/ogg,audio/wav,audio/*;q=0.9,application/ogg;q=0.7,video/*;q=0.6,*/*;q=0.5")
+                .header("Accept-Language", "en-US,en;q=0.5")
+                .header("Range", "bytes=0-")
+                .header("Alt-Used", "v.weeab0o.xyz")
+                .header("Connection", "keep-alive")
+                .header("Referer", "https://japaneseasmr.com/")
+                .header("Sec-Fetch-Dest", "audio")
+                .header("Sec-Fetch-Mode", "no-cors")
+                .header("Sec-Fetch-Site", "cross-site")
+                .header("DNT", "1")
+                .header("Sec-GPC", "1")
+                .header("Accept-Encoding", "identity")
+                .header("TE", "trailers")
                 .send()
-                .await
-                .unwrap();
+                .await;
+            let mut response = match reqwest_result {
+                Ok(response) => response,
+                Err(_) => {
+                    println!("Error downloading {}", filename);
+                    continue;
+                }
+            };
+
             let mut file = async_std::fs::File::create(filename).await?;
             while let Some(chunk) = response.chunk().await.unwrap() {
                 file.write_all(&chunk).await.unwrap();
